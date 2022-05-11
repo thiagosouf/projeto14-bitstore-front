8c7bd6168c7eaff4d7ee2ea3a911723c02bfea95
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-05-11 17:12:34 -0300

    establishing connection to the server

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,14 +1,38 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom"
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import UserContext from '../../contexts/UserContext';
+import UserContext from "../../contexts/UserContext";
 
-export default function HomePage(){
-    const {user} = useContext(UserContext);
-    console.log(user)
-    return(
-        <>  
-            <Link to="/login"><p>Faça Login ou crie seu Cadastro</p></Link>
-        </>
-    )
+export default function HomePage() {
+  const { user } = useContext(UserContext);
+  console.log(user);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const promise = axios.get("http://localhost:5000/products");
+    promise.then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+  if (products.length > 0) {
+    return <>
+    <main>
+        <ul>
+            {products.map((item)=>{
+                return(
+                    <li>
+                        <h1>{item.name}</h1>
+                        <h2>{item.description}</h2>
+                        <h2>{item.price}</h2>
+                    </li>
+                )
+
+
+            })}
+        </ul>
+    </main>
+    </>;
+  }else{
+      return <h1>Carregando</h1>
+  }
 }
