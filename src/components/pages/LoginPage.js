d5dabdc0ba490c199adb2ab55dc78c406c16ec8c
thiagosouf <thiagosouf@gmail.com>
2022-05-11 21:05:39 -0300

    style-components e globalstyles

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import {useState, useContext} from 'react';
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
+import styled from "styled-components"
 
 import UserContext from '../../contexts/UserContext';
 
@@ -13,6 +14,8 @@ export default function SignUpPage() {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [emaillogin, setEmailLogin] = useState('');
     const [passwordlogin, setPasswordLogin] = useState('');
+    const [selecionarLogin, setSelecionarLogin] = useState(false);
+    const [selecionarCadastro, setSelecionarCadastro] = useState(false);
 
     const {setUser} = useContext(UserContext);
 
@@ -68,26 +71,119 @@ export default function SignUpPage() {
 
 
     return(
-        <>
-        <h1>JÁ TENHO CADASTRO</h1>
-        <form onSubmit={login}>
-        {/* <form> */}
-            <input type="email" placeholder="E-mail" value={emaillogin} onChange={e => setEmailLogin(e.target.value)} />
-            <input type="password" placeholder="Senha" value={passwordlogin} onChange={e => setPasswordLogin(e.target.value)} />
-            <button type="submit">Entrar</button>
-        </form>
-        <br></br>
-        <br></br>
-        <h1>QUERO ME CADASTRAR</h1>
-        <form onSubmit={fazerCadastro}>
-        {/* <form> */}
-            <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
-            <input type="number" placeholder="CPF" value={cpf} onChange={(e) => setCpf(e.target.value)} />
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <input type="password" placeholder="Confirmar senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-            <button type="submit">Cadastrar</button>
-        </form>
-        </>
+        <TeladeLogin>
+            <Identificacao>
+                Identificacao
+            </Identificacao>
+            <Formularios>
+                {selecionarCadastro ?
+                    <FormCadastro>
+                    <h1>QUERO ME CADASTRAR</h1>
+                    <Formulario onSubmit={fazerCadastro}>
+                    
+                        <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+                        <input type="number" placeholder="CPF" value={cpf} onChange={(e) => setCpf(e.target.value)} />
+                        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <input type="password" placeholder="Confirmar senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                        <button type="submit">Cadastrar</button>
+                    </Formulario>
+                </FormCadastro>
+                :
+                <FormCadastro onClick={() => setSelecionarCadastro(true)}><h1>FAZER CADASTRO</h1></FormCadastro>
+                    }
+
+                
+                    
+                    {(selecionarLogin)? 
+                        <FormLogin>
+                            <h1>FAZER LOGIN</h1>
+                            <Formulario onSubmit={login}>
+                        
+                            <input type="email" placeholder="E-mail" value={emaillogin} onChange={e => setEmailLogin(e.target.value)} />
+                            <input type="password" placeholder="Senha" value={passwordlogin} onChange={e => setPasswordLogin(e.target.value)} />
+                            <button type="submit">Entrar</button>
+                            </Formulario>
+                        </FormLogin>
+                        : 
+                        <FormLogin onClick={() => setSelecionarLogin(true)}><h1>JA É CADASTRADO?</h1></FormLogin>
+                        }
+                
+                
+            </Formularios>
+        </TeladeLogin>
             );
         }
+
+const TeladeLogin = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #fafafa;
+`;
+const Identificacao = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 30px;
+    width: 90%;
+    font-size: 30px;
+    font-weight: bold;
+    color: black;
+`
+const Formularios = styled.div` 
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`
+const FormCadastro = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    background-color: green;
+    padding: 5px;
+    h1{
+        font-size: 20px;
+        margin-bottom: 30px;
+    }
+`
+
+const FormLogin = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100vw;
+    background-color: green;
+    margin-top: 15px;
+    padding: 5px;
+    h1{
+        font-size: 20px;
+        margin-bottom: 30px;
+    }
+`
+
+const Formulario = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    input{
+        margin-bottom: 8px;
+        font-size: 20px;
+        width: 100%
+    }
+    button{
+        font-size: 20px;
+        font-weight: 700;
+        width: 100%;}
+
+`
